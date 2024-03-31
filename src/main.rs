638e9d468f10eceb700f99b1942d89cd7dd08490
Frans Engström <frans.engstrom@learnet.se>
2024-03-31 21:31:00 +0200

    pass 4nd stage

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
     net::TcpListener,
 };
 
-fn handle_connection(stream: &std::net::TcpStream) -> &'static [u8] {
+fn handle_connection(stream: &std::net::TcpStream) -> Vec<u8> {
     let mut reader = io::BufReader::new(stream);
     let received: Vec<u8> = reader.fill_buf().unwrap().to_vec();
     reader.consume(received.len());
@@ -12,13 +12,23 @@ fn handle_connection(stream: &std::net::TcpStream) -> &'static [u8] {
     let msg = String::from_utf8(received).unwrap();
     let path = msg.lines().nth(0).unwrap();
     match path {
-        "GET / HTTP/1.1" => {
+        path if path.contains("GET") => {
             println!("OK");
-            b"HTTP/1.1 200 OK\r\n\r\n"
+            let route = path
+                .split_whitespace()
+                .nth(1)
+                .unwrap()
+                .split("/")
+                .last()
+                .unwrap();
+            let length = route.len();
+            println!("hmm {:?}", route);
+            let content = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{route}").into_bytes();
+            content
         }
         error => {
             println!("404 {}", error);
-            b"HTTP/1.1 404 Not Found\r\n\r\n"
+            b"HTTP/1.1 404 Not Found\r\n\r\n".to_vec()
         }
     }
 }
@@ -36,7 +46,7 @@ fn main() {
             Ok(mut stream) => {
                 println!("accepted new connection");
                 let msg = handle_connection(&stream);
-                stream.write(msg);
+                stream.write(&msg);
                 // Ok(())
             }
             Err(e) => {
