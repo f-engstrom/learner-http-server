cb4d9b691a446475f8e1f579f73810abde7d2196
Frans Engström <frans.engstrom@learnet.se>
2024-04-01 09:07:57 +0200

    pass 4nd stage

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,44 @@ use std::{
     net::TcpListener,
 };
 
+use itertools::Itertools;
+
+struct Request {
+    method: String,
+    path: String,
+    http_version: String,
+    headers: Vec<String>,
+    body: Option<String>,
+}
+
+fn parse_request(request: String) -> Request {
+    let mut request_lines = request.lines();
+    let request_line = request_lines.next().unwrap();
+    let (method, path, http_version) = request_line.split_whitespace().collect_tuple().unwrap();
+    let headers = [String::from("hej")].to_vec();
+    let req = Request {
+        method: method.to_owned(),
+        path: path.to_owned(),
+        http_version: http_version.to_owned(),
+        headers,
+        body: None,
+    };
+    req
+}
+
 fn handle_connection(stream: &std::net::TcpStream) -> Vec<u8> {
     let mut reader = io::BufReader::new(stream);
     let received: Vec<u8> = reader.fill_buf().unwrap().to_vec();
     reader.consume(received.len());
 
     let msg = String::from_utf8(received).unwrap();
-    let path = msg.lines().nth(0).unwrap();
-    println!("hmm {:?}", path);
+    let req = parse_request(msg);
 
-    match path {
-        path if path.contains("GET") => {
-            println!("OK");
+    println!("hmm {:?}", req.path);
+
+    match req.path {
+        path if path.contains("/echo") => {
+            println!("echo");
             let route = path
                 .split_whitespace()
                 .nth(1)
@@ -28,6 +54,7 @@ fn handle_connection(stream: &std::net::TcpStream) -> Vec<u8> {
             let content = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{route}").into_bytes();
             content
         }
+        path if path.contains("/") => b"HTTP/1.1 200 OK\r\n\r\n".to_vec(),
         error => {
             println!("404 {}", error);
             b"HTTP/1.1 404 Not Found\r\n\r\n".to_vec()
